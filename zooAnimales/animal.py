4f13a4436dfb43e93b925f929011b041a6b79340
Andrés Felipe Muñoz Ortiz <163070299+Andrewxf3@users.noreply.github.com>
2024-12-16 00:03:47 +0000

    17

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,8 +1,3 @@
-from zooAnimales.anfibio import Anfibio
-from zooAnimales.mamifero import Mamifero
-from zooAnimales.ave import Ave
-from zooAnimales.pez import Pez
-from zooAnimales.reptil import Reptil
 class Animal:
     totalAnimales = 0
 
